623849c7220e672121e5ee00d0715f4be4418f27
unknown <khu0mmh@gmail.com>
2024-12-28 13:41:10 +0700

    image

--- backend(ES module)/utils/uploadPhotoProfile.js
+++ backend(ES module)/utils/uploadPhotoProfile.js
@@ -1,5 +1,9 @@
-const multer = require("multer");
-const path = require("path");
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -31,10 +35,10 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({
+const uploadPhoto = multer({
   storage: storage,
   fileFilter: fileFilter,
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-module.exports = upload;
+export default uploadPhoto;
